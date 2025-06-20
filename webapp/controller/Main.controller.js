0db08a30d64d126c1097a0220bad23fe08745850
Meriton Gashi <mgashi@mil.no>
2025-06-20 11:33:33 +0000

    added refersh button

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+], (Controller, MessageBox) => {
     "use strict";
 
     return Controller.extend("no.mil.ztravel.controller.Main", {
         onInit() {
 
+            this.byId("butRefresh").firePress();
+        },
+        
+        onPressRefresh:function(){
             // For managed approuter (e.g. deployed in BTP), use:
             let appId = this.getOwnerComponent().getManifestEntry("/sap.app/id"),
                 appPath = appId.replaceAll(".", "/"),
@@ -14,20 +19,25 @@ sap.ui.define([
             // Local use
             let sUrl = "SAD-SSL/v1/marcopolo";
 
-            var oModel = new sap.ui.model.json.JSONModel();
-            debugger;
+            this.byId("textUpdate").setBusy(true);
             jQuery.ajax({
                 url: sUrl,
                 method: "GET",
                 contentType: "application/json",
-                success: function (oData) {
-                    oModel.setData(oData);
-                    this.getView().setModel(oModel, "marcoPolo");
+                success: function (oData) {                
+
+                    this.getView().setModel(new sap.ui.model.json.JSONModel(oData), "marcoPolo");
+                    this.byId("textUpdate").setBusy(false);
                 }.bind(this),
                 error: function (err) {
                     console.error("API call failed", err);
-                }
+                    this.byId("textUpdate").setBusy(false);
+                }.bind(this)
             });
+        },
+
+        onValueHelpRequest:function(){        
+            new MessageBox.alert("Avventer til ny tjeneste")
         }
     });
 });
